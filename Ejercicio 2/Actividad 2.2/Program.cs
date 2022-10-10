d5cc8229ab8e10320590e0a84d3fb61c903a9a4d
Ignacio Corsini <105394534+NachoCorsini@users.noreply.github.com>
2022-10-10 14:42:36 -0300

    Add files via upload

---
+++ Ejercicio 2/Actividad 2.2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actividad_2._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           //EJERCICIO 3 DE STRING
+            
+            string nombre; string apellido;
+            int dni; string mensaje;
+            do
+            {
+                Console.WriteLine("Ingrese su nombre");
+                nombre = Console.ReadLine();
+            } while (nombre == "");
+
+            do
+            {
+                Console.WriteLine("Ingrese su apellido");
+                apellido = Console.ReadLine();
+            } while (apellido == "");
+
+            do
+            {
+                Console.WriteLine("Ingrese su DNI");
+            } while (!int.TryParse(Console.ReadLine(), out dni) ) ;
+
+            mensaje = String.Concat("NOMBRE: ", nombre, " \nAPELLIDO: ", apellido, " \nD.N.I: ", dni);
+            Console.WriteLine(mensaje);
+
+            Console.ReadKey();
+        }
+
+
+
+
+
+
+    }
+
+
+
+}
---
+++ Ejercicio 2/actividad 2/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace actividad_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //EJERCICIO 2 DE STRING
+
+            string nombre; string apellido; string password;
+
+            do
+            {
+                Console.WriteLine("Ingrese su nombre");
+                nombre = Console.ReadLine();
+            } while (nombre == "");
+
+            apellido = ChequearApellido("Ingrese apellido");
+
+            Console.WriteLine("Ingrese clave de acceso");
+            password = Console.ReadLine();
+
+            if (password != "demo") Console.WriteLine("ACCESO DENEGADO");
+            else Console.WriteLine(String.Format("ACCESO CONCEDIDO A: {0} {1}", apellido, nombre));
+
+
+            Console.ReadKey();
+        }
+
+        static string ChequearApellido(string ap)
+        {
+            string name2;
+            do
+            {
+                Console.WriteLine(ap);
+                name2 = Console.ReadLine();
+
+            } while (name2 == "");
+
+            return name2;
+        }
+
+
+
+
+
+    }
+
+
+
+    
+}
