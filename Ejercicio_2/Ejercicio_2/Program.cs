c9515c6cf0721822d83d834053bb686dd06d8683
Ignacio Corsini <105394534+NachoCorsini@users.noreply.github.com>
2022-10-10 14:44:02 -0300

    Add files via upload

---
+++ Ejercicio_2/Ejercicio_2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Ejercicio_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char char1 = ' ';
+            char salida = ' ';
+            bool respuesta = false;
+
+            do
+            {
+                do
+                {
+                    Console.WriteLine("\nIngrese un caracter");
+                    respuesta = char.TryParse(Console.ReadLine(), out char1);
+
+                } while (respuesta == false);
+                
+                Console.WriteLine($"Categoría del caracter: {char.GetUnicodeCategory(char1)}");
+
+
+                Console.WriteLine("\n¿Desea seguir evaluando caracteres? Oprima 's' para continuar ó 'n' para salir");
+                respuesta = char.TryParse(Console.ReadLine(), out salida);
+                Console.ReadKey();
+
+            } while (salida == 's' || salida == 'S');
+
+
+
+
+           
+        }
+
+
+
+
+    }
+}
---
+++ Ejercicio_3/Ejercicio_3/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Ejercicio_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char char1, respuesta;
+            bool respCorrecta;
+
+            do
+            {
+
+                do
+                {
+                    Console.WriteLine("Ingrese un caracter");
+                    respCorrecta = char.TryParse(Console.ReadLine(), out char1);
+
+                } while (respCorrecta == false);
+
+                if (char.IsNumber(char1)) Console.WriteLine($"El doble del numero ingresado es: {char.GetNumericValue(char1) * 2} ");
+                if (char.IsLetter(char1)) Console.WriteLine($"El caracter ingresado es una letra: {char1}");
+                if (!char.IsLetter(char1) && !char.IsNumber(char1)) Console.WriteLine($"El caracter ingresado pertenece a: {char.GetUnicodeCategory(char1)}");
+
+
+                Console.WriteLine("\nSi desea CONTINUAR presione 'S' o 's'");
+                respCorrecta = char.TryParse(Console.ReadLine(), out respuesta);
+
+            } while (respuesta.Equals('S') || respuesta.Equals('s'));
+
+            Console.ReadKey();
+        }
+
+
+
+
+
+
+    }
+}
